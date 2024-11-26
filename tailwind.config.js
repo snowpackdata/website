e4744a4da5abdfc452e5d9b6c594c9446f81732a
Nate Robinson <nathanaeljrobinson@gmail.com>
2024-11-26 12:16:38 -0800

    add new colors and about page updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,21 @@ module.exports = {
   theme: {
     colors: {
       'teal': '#226d87',
+      'light-blue': '#ced9e1',
       'blue': '#021c51',
-      'pink': '#ff49db',
+      'dark-blue': '#0a192f',
+      'pink': '#be5bda',
       'orange': '#ff7849',
+      'light-green': '#b5e7a0',
       'green': '#58837e',
       'gray-dark': '#22262a',
       'gray': '#d6d7d9',
       'gray-light': '#eeeff1',
       'white': '#ffffff',
       'yellow' : '#f7d070',
+      'red': '#9f0d0d',
+      'black': '#000000',
+
     },
     fontFamily: {
       sans: ['Montserrat', 'sans-serif'],
