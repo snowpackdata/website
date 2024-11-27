db884fd5e1b5b45015e46182f66af27a8e9d211c
Danny Blumenthal <dblumenthal@Dannys-MacBook-Pro.local>
2024-11-27 12:52:26 -0800

    update colors in config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,42 @@ module.exports = {
   theme: {
     colors: {
       'teal': '#226d87',
-      'blue': '#021c51',
-      'pink': '#ff49db',
+      'blue': {
+        DEFAULT: '#021c51',
+        'light-blue': '#ced9e1',
+        'dark-blue': '#0a192f',
+      },
+      'pink': '#be5bda',
       'orange': '#ff7849',
-      'green': '#58837e',
-      'gray-dark': '#22262a',
-      'gray': '#d6d7d9',
-      'gray-light': '#eeeff1',
+      'green': {
+        DEFAULT: '#58837e',
+        'light-green': '#b5e7a0',
+        100: "#dcfce7",
+        600: "#16a34a"
+      },
+      'gray': {
+        DEFAULT: '#d6d7d9',
+        'gray-light': '#eeeff1',
+        'gray-dark': '#22262a',
+        400: '#9ca3af',
+        500: '#6b7280',
+        900: "#111827"
+      },
       'white': '#ffffff',
       'yellow' : '#f7d070',
+      'black': '#000000',
+      'red': {
+        DEFAULT: '#9f0d0d',
+        600: "#dc2626",
+        700: "#b91c1c"
+      },
+      'sky': {
+        600: "#0284c7",
+        700: "#0369a1",
+        800: "#075985",
+        900: "#0c4a6e",
+        950: "#082f49"
+      }
     },
     fontFamily: {
       sans: ['Montserrat', 'sans-serif'],
@@ -25,4 +52,3 @@ module.exports = {
       require('@tailwindcss/forms'),
     ]
 }
-
